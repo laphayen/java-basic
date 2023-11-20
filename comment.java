98dc8c40e62067a5a75ade975c84d6a9a55173c6
Baek Gi Chan <laphayen@gmail.com>
2023-11-20 23:56:30 +0900

    Upload comment.java

---
+++ comment.java
@@ -0,0 +1,17 @@
+package chap01;
+
+public class Hello {
+	public static void main(String[] arp) {
+		// 행 단위 주석
+		
+		/*
+		 * 블록 단위 주
+		 */
+		
+		/**
+		 * 문서화 주석 - 클래스, 인터페이스 멤버 각각 하니만 작성, 선언하기 바로 전에 작
+		 */
+		
+		System.out.println("Hello world!");
+	}
+}
