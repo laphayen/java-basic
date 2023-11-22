b342459cdb43b31b1eba3bee5034df069a77c8ba
Baek Gi Chan <laphayen@gmail.com>
2023-11-22 23:52:13 +0900

    Upload variable.java

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        // 변수 선언
+        int num1;
+        num1 = 10;
+        num2 = 20;
+        System.out.println(count);
+        // 변수 선언과 초기화
+        double pi = 3.14;
+        System.out.println(pi);
+    }
+}
