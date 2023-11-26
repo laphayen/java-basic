de1b06ab30f5f79ec4f0c6f0d5a9da3143826745
Baek Gi Chan <laphayen@gmail.com>
2023-11-26 16:43:31 +0900

    Upload .java

--- comment.java
+++ comment.java
@@ -5,13 +5,19 @@ public class Hello {
 		// 행 단위 주석
 		
 		/*
-		 * 블록 단위 주
+		 * 블록 단위 주석
 		 */
 		
 		/**
-		 * 문서화 주석 - 클래스, 인터페이스 멤버 각각 하니만 작성, 선언하기 바로 전에 작
+		 * 문서화 주석 - 클래스, 인터페이스 멤버 각각 하나만 작성, 선언하기 바로 전에 작성
+		 * add 메소드는 두 수를 더합니다.
+		 * @param a 첫 번째 정수
+		 * @param b 두 번째 정수
+		 * @return 두 정수의 합
 		 */
-		
-		System.out.println("Hello world!");
+		public int add(int a, int b)
+		{
+			return a + b;
+		}
 	}
 }
