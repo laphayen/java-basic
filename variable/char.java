0f04d6b53c100646e5a0477276e27afbd27ca7ae
Baek Gi Chan <laphayen@gmail.com>
2023-11-27 17:18:39 +0900

    Update char.java

--- variable/char.java
+++ variable/char.java
@@ -3,7 +3,9 @@ public class Example {
 	
 	public static void main(String[] args) {
 		char myChar = 'A';
-		int myInt = myChar;
-		System.out.println(myInt);
+		int myInt1 = myChar;
+		int myInt2 = myChar + 1;
+		System.out.println(myInt1);
+		System.out.println(myInt2);
 	}
 }
