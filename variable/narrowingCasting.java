cd7131b5c4ee872da1e14539ef36a298e361902a
Baek Gi Chan <laphayen@gmail.com>
2023-11-30 20:17:31 +0900

    Upload narrowingCasting.java

---
+++ variable/narrowingCasting.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Example {
+	public static void main(String[] args) {
+		double doubleVar = 1.1618;
+		int intVar = (int) doubleVar;
+		System.out.println(intVar);
+	}
+}
