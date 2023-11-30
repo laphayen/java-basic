179144c132cb1b71cbfa246a5dc2c608bc07af7c
Baek Gi Chan <laphayen@gmail.com>
2023-11-30 20:17:02 +0900

    Upload arithmeticOperators.java

---
+++ operators/arithmeticOperators.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Example {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		int sum = a + b;
+		int difference = a - b;
+		int product = a * b;
+		int division = a / b;
+		int remainder = a % b;
+		System.out.println(sum);
+		System.out.println(difference);
+		System.out.println(product);
+		System.out.println(division);
+		System.out.println(remainder);
+	}
+}
