2306bfa23629d7513479f65d618f0c0d31ebffa4
Baek Gi Chan <laphayen@gmail.com>
2023-11-30 20:17:21 +0900

    comparisonOperators.java

---
+++ operators/comparisonOperators.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Example {
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 3;
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a < b);
+		System.out.println(a <= b);
+		System.out.println(a > b);
+		System.out.println(a >= b);
+	}
+}
